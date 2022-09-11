75cf909a073acc9a6741ef6b18b66dd0ac895fc6
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-09-11 23:02:48 +0530

    react

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const canvas=document.querySelector("canvas");
 ctx=canvas.getContext("2d");
+toolbtn=document.querySelectorAll('.tool');
+
+let prevMouseX,prevMouseY,
+isDrwaing=false;
+selectedTool='brush'
+brushWidth=5;
 
 window.addEventListener("load",()=>{
     canvas.width=canvas.offsetWidth;
     canvas.height=canvas.offsetHeight;
 })
 
-const drawing=(e)=>{
-    ctx.lineTo(e.offsetX,e.offsetY);
-    ctx.stroke();
+const startDraw=(e)=>{
+     isDrwaing=true;
+     prevMouseX=e.offsetX;
+     prevMouseY=e.offsetY;
+     ctx.beginPath();
+     ctx.lineWidth=brushWidth;
+}
+const drawRect=(e)=>{
+   ctx.strokeRect(e.offsetX,e.offsetY,prevMouseX-e.offsetX,prevMouseY-e.offsetY);
 }
 
+const drawing=(e)=>{
+    if(!isDrwaing) return;
+    if(selectedTool==='brush'){
+        ctx.lineTo(e.offsetX,e.offsetY);
+        ctx.stroke();
+    }
+    else if(selectedTool==='reactangle'){
+         drawRect(e)
+    }
+}
+
+toolbtn.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        document.querySelector('.options .active').classList.remove('active');
+        btn.classList.add('active');
+        selectedTool=btn.id;
+        console.log(selectedTool)
+    });
+})
+
+
+
+canvas.addEventListener("mousedown",startDraw)
+
 canvas.addEventListener("mousemove",drawing)
+canvas.addEventListener("mouseup",()=>isDrwaing=false);
